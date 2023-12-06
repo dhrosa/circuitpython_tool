bb56372f213d46880acde1f4ea727c211ccbfea7
Diony Rosa <dhrosa@gmail.com>
2023-12-06 00:52:11 -0500

    Simplify naming of locals in parse_args()

--- circuitpython_tool/args.py
+++ circuitpython_tool/args.py
@@ -4,65 +4,59 @@ from pathlib import Path
 
 def parse_args():
     """Parse command line arguments."""
-    parser = ArgumentParser()
+    root = ArgumentParser()
 
     # Subcommand parsers.
-    subparsers = parser.add_subparsers(title="commands", required=True, dest="command")
+    children = root.add_subparsers(title="commands", required=True, dest="command")
 
-    subparsers.add_parser("devices", help="List all connected CircuitPython devices.")
+    children.add_parser("devices", help="List all connected CircuitPython devices.")
 
-    upload_parser = subparsers.add_parser("upload", help="Upload code to device.")
-    upload_parser.add_argument("preset_name", type=str)
-    upload_parser.add_argument(
+    upload = children.add_parser("upload", help="Upload code to device.")
+    upload.add_argument("preset_name", type=str)
+    upload.add_argument(
         "--watch",
         "-w",
         action="store_true",
         help="Continuously upload code as files in the source directories change.",
     )
 
-    connect_parser = subparsers.add_parser(
-        "connect", help="Connect to device's serial console."
-    )
-    connect_parser.add_argument("preset_name", type=str)
+    connect = children.add_parser("connect", help="Connect to device's serial console.")
+    connect.add_argument("preset_name", type=str)
 
-    preset_list_parser = subparsers.add_parser(
-        "preset_list", help="List existing presets."
-    )
+    preset_list = children.add_parser("preset_list", help="List existing presets.")
 
-    preset_save_parser = subparsers.add_parser(
-        "preset_save", help="List existing presets."
-    )
-    preset_save_parser.add_argument(
+    preset_save = children.add_parser("preset_save", help="List existing presets.")
+    preset_save.add_argument(
         "new_preset_name", type=str, help="Name of preset to save."
     )
-    preset_save_parser.add_argument(
+    preset_save.add_argument(
         "source_dir",
         type=Path,
         nargs="+",
         help="Source directory to copy.",
     )
-    preset_save_parser.add_argument(
+    preset_save.add_argument(
         "--vendor",
         "-v",
         type=str,
         default="",
         help="Filter to devices whose vendor contains this string.",
     )
-    preset_save_parser.add_argument(
+    preset_save.add_argument(
         "--model",
         "-m",
         type=str,
         default="",
         help="Filter to devices whose model contains this string.",
     )
-    preset_save_parser.add_argument(
+    preset_save.add_argument(
         "--serial",
         "-s",
         type=str,
         default="",
         help="Filter to devices whose serial contains this string.",
     )
-    preset_save_parser.add_argument(
+    preset_save.add_argument(
         "--preset",
         "-p",
         type=str,
@@ -71,7 +65,7 @@ def parse_args():
     )
 
     # Ensure these attributes are set even if the relevant commands aren't specified.
-    parser.set_defaults(
+    root.set_defaults(
         source_dir=[],
         watch=False,
         new_preset_name="",
@@ -82,4 +76,4 @@ def parse_args():
         preset_name="",
     )
 
-    return parser.parse_args()
+    return root.parse_args()

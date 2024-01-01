9a5cb4967c3cd2ac89c86bb16ec50f1a1d938b2f
Diony Rosa <dhrosa@gmail.com>
2024-01-01 16:03:21 -0500

    Start fake_device library for use in demos and tests

---
+++ src/circuitpython_tool/fake_device.py
@@ -0,0 +1,54 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+import tomlkit
+from tomlkit.items import AoT, Table
+
+from .device import Device
+
+
+@dataclass
+class FakeDevice(Device):
+    """Fake Device implementation for use in tests and demos."""
+
+    mountpoint: Path | None = None
+
+    def get_mountpoint(self) -> Path | None:
+        return self.mountpoint
+
+    def mount_if_needed(self) -> Path:
+        path = self.get_mountpoint()
+        assert path
+        return path
+
+    @staticmethod
+    def from_toml(table: Table) -> "FakeDevice":
+        def get(key: str) -> str:
+            value = table[key]
+            assert isinstance(value, str)
+            return value
+
+        def get_optional_path(key: str) -> Path | None:
+            value = table.get(key)
+            if isinstance(value, str):
+                return Path(value)
+            return value
+
+        return FakeDevice(
+            vendor=get("vendor"),
+            model=get("model"),
+            serial=get("serial"),
+            partition_path=get_optional_path("partition_path"),
+            serial_path=get_optional_path("serial_path"),
+            mountpoint=get_optional_path("mountpoint"),
+        )
+
+
+def all_devices(toml: str | Path) -> list[FakeDevice]:
+    """Load FakeDevice objects from a TOML file."""
+    if isinstance(toml, Path):
+        toml = toml.read_text()
+    document = tomlkit.loads(toml)
+    tables = document.get("devices", tomlkit.aot())
+    assert isinstance(tables, AoT)
+    return [FakeDevice.from_toml(t) for t in tables]
---
+++ tests/test_fake_device.py
@@ -0,0 +1,71 @@
+from pathlib import Path
+
+import pytest
+
+from circuitpython_tool.fake_device import FakeDevice, all_devices
+
+
+def test_empty_str() -> None:
+    assert all_devices("") == []
+
+
+def test_required_fields() -> None:
+    toml = """
+[[devices]]
+vendor = "a"
+model = "b"
+serial = "c"
+
+[[devices]]
+vendor = "d"
+model = "e"
+serial = "f"
+    """
+    assert all_devices(toml) == [FakeDevice("a", "b", "c"), FakeDevice("d", "e", "f")]
+
+
+def test_missing_required_fields() -> None:
+    toml = """
+[[devices]]
+vendor = "v"
+# model = "m"
+serial = "s"
+   """
+    with pytest.raises(KeyError) as exception_info:
+        all_devices(toml)
+    assert "model" in str(exception_info.value)
+
+
+def test_optional_fields() -> None:
+    toml = """
+[[devices]]
+vendor = "v"
+model = "m"
+serial = "s"
+
+serial_path = "/serial"
+partition_path = "/partition"
+mountpoint = "/mount"
+   """
+    all_devices(toml) == [
+        FakeDevice(
+            "v",
+            "m",
+            "s",
+            serial_path=Path("/serial"),
+            partition_path=Path("/partition"),
+            mountpoint=Path("/mount"),
+        ),
+    ]
+
+
+def test_file_read(tmp_path: Path) -> None:
+    toml = """
+[[devices]]
+vendor = "v"
+model = "m"
+serial = "s"
+      """
+    file_path = tmp_path / "devices.toml"
+    file_path.write_text(toml)
+    assert all_devices(file_path) == [FakeDevice("v", "m", "s")]

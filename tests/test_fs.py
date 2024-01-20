960e49a78522f3a0fe140539c6713d9a62bc520b
Diony Rosa <dhrosa@gmail.com>
2024-01-19 21:27:40 -0500

    Add unit test for fs.upload

--- tests/test_fs.py
+++ tests/test_fs.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from circuitpython_tool.fs import guess_source_dir, walk
+from circuitpython_tool.fs import guess_source_dir, upload, walk
 
 
 def test_guess_source_dir_empty_dir(tmp_path: Path) -> None:
@@ -40,20 +40,21 @@ def test_guess_source_dir_no_code_file(tmp_path: Path) -> None:
     assert guess_source_dir(tmp_path) is None
 
 
-def test_walk(tmp_path: Path) -> None:
-    def create_path(path_str: str) -> None:
-        path = tmp_path / path_str
-        if path_str.endswith("/"):
-            path.mkdir(parents=True)
-        else:
-            path.parent.mkdir(exist_ok=True)
-            path.touch()
+def create_file(base_dir: Path, path_str: str) -> None:
+    path = base_dir / path_str
+    path.parent.mkdir(exist_ok=True)
+    path.touch()
 
-    create_path("file.txt")
-    create_path("a/file1.txt")
-    create_path("a/file2.txt")
-    create_path("a/b/file.txt")
-    create_path("c/file.txt")
+
+def test_walk(tmp_path: Path) -> None:
+    for p in (
+        "file.txt",
+        "a/file1.txt",
+        "a/file2.txt",
+        "a/b/file.txt",
+        "c/file.txt",
+    ):
+        create_file(tmp_path, p)
 
     # Strip off temporary path prefix for stable output for failure messages and simpler assertions.
     entries = [str(p.relative_to(tmp_path)) for p in walk(tmp_path)]
@@ -69,5 +70,51 @@ def test_walk(tmp_path: Path) -> None:
             "a/b/file.txt",
             "c",
             "c/file.txt",
+        ]
+    )
+
+
+def test_upload_single_dir(tmp_path: Path) -> None:
+    source_dir = tmp_path / "source_dir"
+    source_dir.mkdir()
+    mountpoint = tmp_path / "mountpoint"
+    mountpoint.mkdir()
+
+    create_file(source_dir, "top.txt")
+    create_file(source_dir, "sub/sub.txt")
+
+    upload([source_dir], mountpoint)
+
+    entries = [str(p.relative_to(mountpoint)) for p in walk(mountpoint) if p.is_file()]
+
+    assert sorted(entries) == sorted(
+        [
+            "top.txt",
+            "sub/sub.txt",
+        ]
+    )
+
+
+def test_upload_multiple_dirs(tmp_path: Path) -> None:
+    source_dir_a = tmp_path / "source_a"
+    source_dir_a.mkdir()
+    (source_dir_a / "a.txt").touch()
+
+    source_dir_b = tmp_path / "source_b"
+    source_dir_b.mkdir()
+    (source_dir_b / "b.txt").touch()
+
+    mountpoint = tmp_path / "mountpoint"
+    mountpoint.mkdir()
+
+    upload([source_dir_a, source_dir_b], mountpoint)
+
+    entries = [str(p.relative_to(mountpoint)) for p in walk(mountpoint) if p.is_file()]
+
+    # Source directory contents should be merged.
+    assert sorted(entries) == sorted(
+        [
+            "a.txt",
+            "b.txt",
         ]
     )

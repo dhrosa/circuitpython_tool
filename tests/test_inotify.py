5ab4e3b56aad13448e4c5b208b011e1ac3284efc
Diony Rosa <dhrosa@gmail.com>
2024-01-24 16:26:05 -0500

    Cleanup inotify module.

--- tests/test_inotify.py
+++ tests/test_inotify.py
@@ -1,21 +1,28 @@
 import asyncio
 from pathlib import Path
 
-from circuitpython_tool.inotify import INotify, flags
+from circuitpython_tool.inotify import INotify, Mask
 
 
-def test_lol(tmp_path: Path) -> None:
-    watcher = INotify()
-
-    assert watcher.fd > 2
-    watcher.add_watch(tmp_path, flags.CREATE | flags.MODIFY)
-
+def test_file_changes(tmp_path: Path) -> None:
     async def body() -> None:
+        (tmp_path / "existing.txt").touch()
+
+        watcher = INotify()
+        watcher.add_watch(tmp_path, Mask.CREATE | Mask.MODIFY)
+
         events = watcher.events()
-        (tmp_path / "create.txt").touch()
-        async with asyncio.timeout(2):
-            assert (await anext(events)).path.relative_to(tmp_path) == Path(
-                "create.txt"
-            )
+        async with asyncio.timeout(1):
+            (tmp_path / "created.txt").touch()
+            event = await anext(events)
+            assert event.path.relative_to(tmp_path) == Path("created.txt")
+            assert Mask.CREATE in event.mask
+            assert Mask.MODIFY not in event.mask
+
+            (tmp_path / "existing.txt").write_text("new contents")
+            event = await anext(events)
+            assert event.path.relative_to(tmp_path) == Path("existing.txt")
+            assert Mask.CREATE not in event.mask
+            assert Mask.MODIFY in event.mask
 
     asyncio.run(body(), debug=True)

9ff11d6e82a862f13a9443a06ce2321237528e19
Diony Rosa <dhrosa@gmail.com>
2024-01-25 20:47:16 -0500

    Replace Python 3.11 asyncio.timeout() with asyncio.wait_for

--- tests/test_inotify.py
+++ tests/test_inotify.py
@@ -12,17 +12,16 @@ def test_file_changes(tmp_path: Path) -> None:
         watcher.add_watch(tmp_path, Mask.CREATE | Mask.MODIFY)
 
         events = watcher.events()
-        async with asyncio.timeout(1):
-            (tmp_path / "created.txt").touch()
-            event = await anext(events)
-            assert event.path.relative_to(tmp_path) == Path("created.txt")
-            assert Mask.CREATE in event.mask
-            assert Mask.MODIFY not in event.mask
+        (tmp_path / "created.txt").touch()
+        event = await anext(events)
+        assert event.path.relative_to(tmp_path) == Path("created.txt")
+        assert Mask.CREATE in event.mask
+        assert Mask.MODIFY not in event.mask
 
-            (tmp_path / "existing.txt").write_text("new contents")
-            event = await anext(events)
-            assert event.path.relative_to(tmp_path) == Path("existing.txt")
-            assert Mask.CREATE not in event.mask
-            assert Mask.MODIFY in event.mask
+        (tmp_path / "existing.txt").write_text("new contents")
+        event = await anext(events)
+        assert event.path.relative_to(tmp_path) == Path("existing.txt")
+        assert Mask.CREATE not in event.mask
+        assert Mask.MODIFY in event.mask
 
-    asyncio.run(body(), debug=True)
+    asyncio.run(asyncio.wait_for(body(), timeout=1), debug=True)

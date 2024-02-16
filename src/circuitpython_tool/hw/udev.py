ec95b1c46ca5e6b36d387de768e5d55212fec3ca
Diony Rosa <dhrosa@gmail.com>
2024-02-16 12:07:50 -0500

    Add udev.UsbDevice for atomically iterating over udev entries.
    Using `udevadm info --export-db` lets us iterate over an atomic snapshot of
    attached devices without a TOCTOU bug.

--- src/circuitpython_tool/hw/udev.py
+++ src/circuitpython_tool/hw/udev.py
@@ -1,6 +1,12 @@
+from collections.abc import Iterator
+from dataclasses import dataclass
+from logging import getLogger
 from pathlib import Path
 
+from ..iter import as_list
 from .shell import run
+
+logger = getLogger(__name__)
 
 
 def usb_device_properties(path: Path) -> dict[str, str] | None:
@@ -26,3 +32,59 @@ def udevadm_info(path: Path) -> str:
     Separated out for patching in unit tests.
     """
     return run(f"udevadm info --query=property --name {path}")
+
+
+def udevadm_export_db() -> str:
+    """Dump information on all attached devices.
+
+    Separated out for patching in unit tests.
+
+    Output format of udevadm documented at:
+    https://man7.org/linux/man-pages/man8/udevadm.8.html#:~:text=Table%201.%20udevadm%20info%20output%20prefixes
+    """
+    return run("udevadm info --export-db")
+
+
+@dataclass
+class UsbDevice:
+    """USB device properties from udev."""
+
+    path: Path
+    vendor: str
+    model: str
+    serial: str
+
+    partition_label: str | None
+    """If this is a partition device, the filesystem label."""
+
+    @as_list
+    @staticmethod
+    def all() -> Iterator["UsbDevice"]:
+        """List of all attached USB devices."""
+        # Entries have one blank line between them.
+        for entry in udevadm_export_db().rstrip().split("\n\n"):
+            properties = parse_properties(entry)
+            if properties.get("ID_BUS") != "usb":
+                continue
+            yield UsbDevice(
+                path=Path(properties["DEVPATH"]),
+                vendor=properties["ID_USB_VENDOR"],
+                model=properties["ID_USB_MODEL"],
+                serial=(
+                    properties.get("ID_USB_SERIAL_SHORT") or properties["ID_USB_SERIAL"]
+                ),
+                partition_label=properties.get("ID_FS_LABEL"),
+            )
+
+
+def parse_properties(entry: str) -> dict[str, str]:
+    """Parse device properties from a udev database entry."""
+    properties: dict[str, str] = {}
+    for original_line in entry.splitlines():
+        # Only pay attention to device property lines (prefix "E:")
+        line = original_line.removeprefix("E: ")
+        if line == original_line:
+            continue
+        key, value = line.split("=", maxsplit=1)
+        properties[key] = value
+    return properties
---
+++ tests/hw/test_udev.py
@@ -0,0 +1,99 @@
+from pathlib import Path
+
+from pytest import MonkeyPatch, fixture
+
+from circuitpython_tool.hw import udev
+
+UsbDevice = udev.UsbDevice
+
+
+@fixture(autouse=True)
+def fake_udev(monkeypatch: MonkeyPatch) -> list[list[str]]:
+    """Inject fake `udevadm info --export_db` implementation.
+
+    Each entry corresponds to a sublist in this fixture's output. Each sublist
+    is a list of lines.
+    """
+    entries: list[list[str]] = []
+
+    def fake_export_db() -> str:
+        return "\n\n".join("\n".join(e) for e in entries)
+
+    monkeypatch.setattr(udev, "udevadm_export_db", fake_export_db)
+    return entries
+
+
+def test_empty_db() -> None:
+    assert udev.UsbDevice.all() == []
+
+
+def test_db_without_usb_entries(fake_udev: list[list[str]]) -> None:
+    fake_udev.append(
+        [
+            "E: DEVPATH=/devices/no_properties",
+        ]
+    )
+    fake_udev.append(
+        [
+            "E: DEVPATH=/devices/non_usb_bus",
+            "E: ID_BUS=pci",
+        ]
+    )
+    assert udev.UsbDevice.all() == []
+
+
+def test_db_usb_entries(fake_udev: list[list[str]]) -> None:
+    fake_udev.append(
+        [
+            "E: DEVPATH=/devices/with_serial",
+            "E: ID_BUS=usb",
+            "E: ID_USB_VENDOR=vendor1",
+            "E: ID_USB_MODEL=model1",
+            "E: ID_USB_SERIAL=serial1",
+        ]
+    )
+
+    fake_udev.append(
+        [
+            "E: DEVPATH=/devices/with_short_serial",
+            "E: ID_BUS=usb",
+            "E: ID_USB_VENDOR=vendor2",
+            "E: ID_USB_MODEL=model2",
+            "E: ID_USB_SERIAL_SHORT=serial2",
+        ]
+    )
+
+    fake_udev.append(
+        [
+            "E: DEVPATH=/devices/with_fs_label",
+            "E: ID_BUS=usb",
+            "E: ID_USB_VENDOR=vendor3",
+            "E: ID_USB_MODEL=model3",
+            "E: ID_USB_SERIAL=serial3",
+            "E: ID_FS_LABEL=fs_label",
+        ]
+    )
+
+    assert UsbDevice.all() == [
+        UsbDevice(
+            path=Path("/devices/with_serial"),
+            vendor="vendor1",
+            model="model1",
+            serial="serial1",
+            partition_label=None,
+        ),
+        UsbDevice(
+            path=Path("/devices/with_short_serial"),
+            vendor="vendor2",
+            model="model2",
+            serial="serial2",
+            partition_label=None,
+        ),
+        UsbDevice(
+            path=Path("/devices/with_fs_label"),
+            vendor="vendor3",
+            model="model3",
+            serial="serial3",
+            partition_label="fs_label",
+        ),
+    ]
